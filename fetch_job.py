162edb2b4c854e0a175b5f4583b10130c900b73f
waseem0296 <waseemibneallahdittah@gmail.com>
2025-12-15 01:33:51 +0500

    fetch Job updated

--- fetch_job.py
+++ fetch_job.py
@@ -1,38 +1,3 @@
-# import requests
-# import csv
-
-# # API details
-# url = "https://api.hireology.com/ats/v3/org/ac036f48-f44d-41b8-a1c7-fcd60b9d60bd/search/jobs"
-
-# headers = {
-#     "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlJNblFlZmkwZFB5emRIdFR0Z2xrVmdFWGp0SHFzSEFkb0Jjbi1nMjJJb1UiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJiMWUwZTc4Yy05MGU3LTQxOTgtOTU5NC0zMDI3ZWZkN2Y3MTAiLCJpc3MiOiJodHRwczovL2FjY291bnRzLmhpcmVvbG9neS5jb20vNDZlODJkZWEtOTFmNC00ZDMwLWFhYmMtYWMxZWUwMzBiZWMyL3YyLjAvIiwiZXhwIjoxNzY1NzQ2MDYwLCJuYmYiOjE3NjU3NDI0NjAsInN1YiI6IjIzMGE5NjZhLTc4Y2ItNGMzOS1hZGEzLTdlODkyNWNjNmQ5MSIsIm9pZCI6IjIzMGE5NjZhLTc4Y2ItNGMzOS1hZGEzLTdlODkyNWNjNmQ5MSIsInRpZCI6IjQ2ZTgyZGVhLTkxZjQtNGQzMC1hYWJjLWFjMWVlMDMwYmVjMiIsImttc2kiOiJGYWxzZSIsInNjcCI6InYyIGFpIHYzIiwiYXpwIjoiZjA2N2U2YTMtODE1ZC00MTNkLTg3OGUtZDIyM2VmYWEwY2E0IiwidmVyIjoiMS4wIiwiaWF0IjoxNzY1NzQyNDYwfQ.uNCbV2IVtbjB-1ojUFIAyG59fWJt6WV8yUIfOH9jdUDzFZLrFpbHCcR7DNkH5w7BGNn80mjrMFfbZqQvVKvAdKAjcnhXj1lpyVHUhcRlFt2x7MPemnM-iDQJ_iLB8wdX815e_LnuhAAf3bd15sp7e2dTjSHpwIsp6rggbRAhjTNfRzdGuXp77z8Ne7MThksGQBogWjSbajBhIaNkoKjo9FVq_EgHoB7PuUZyBaVZF89u4kJQ7TkQzv6yvRtybLygm13EHbGaDqS1lNncA009yAySadohvGdQdLUa4oTGzF_fSz7CFbYrBODLrH6G4K2rDIHOaL0soJpcnLcXpZ31Kw",
-#     "Accept": "application/json",
-#     "Content-Type": "application/json"
-# }
-
-# payload = {}
-
-# # API call
-# response = requests.post(url, headers=headers, json=payload)
-
-# response.raise_for_status()
-
-# data = response.json()
-
-# # Extract jobs
-# jobs = data.get("data", [])
-
-# # Write to CSV
-# with open("jobs.csv", mode="w", newline="", encoding="utf-8") as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["Job ID", "Job Name"])
-
-#     for job in jobs:
-#         writer.writerow([job.get("id"), job.get("name")])
-
-# print("✅ jobs.csv created successfully")
-
-
 import requests
 import csv
 
@@ -50,7 +15,7 @@ response = requests.post(url, headers=headers, json=payload)
 response.raise_for_status()
 
 data = response.json()
-
+print("Data ", data)
 jobs = data.get("jobs", [])
 
 print(f"Total jobs found: {len(jobs)}")
@@ -64,7 +29,7 @@ with open("jobs.csv", mode="w", newline="", encoding="utf-8") as file:
             job.get("legacyId"),
             job.get("id"),
             job.get("title"),
-            job.get("newApplicantCount", 0),
+            job.get("applicantCount", 0),
             job.get("candidateCount", 0)
         ])
 
